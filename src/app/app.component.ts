311bf4c260ab58e7617fef711fb7982cfcf0c491
Smyk <tomasz.jeziorny@wp.pl>
2020-03-25 11:07:50 +0100

    Hide and show add panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Quotation } from './models/quotation';
+import { QUOTES } from './models/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  HIDDEN = 'hidden';
+  VISIBLE = 'visible';
+  addPanelVisibilityStatus = this.HIDDEN;
+  showAddForm = false;
+  quotations: Quotation[] = QUOTES;
+
+  changeAddPanelVisibility() {
+    if (this.addPanelVisibilityStatus === this.HIDDEN) {
+      this.addPanelVisibilityStatus = this.VISIBLE;
+    } else {
+      this.addPanelVisibilityStatus = this.HIDDEN;
+    }
+  }
+
+  changeShowAddForm(){
+    this.showAddForm = !this.showAddForm;
+  }
+
+}
